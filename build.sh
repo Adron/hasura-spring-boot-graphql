fcfe46a91d7aec9b9d87b2ba5c411e436a280c1a
Adron Hall <adron@thrashingcode.com>
2023-04-23 13:13:37 -0500

    Fixed the build and added some comments for clean up. Removed the Hasura stuff since it is not needed in this repo.

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 ./gradlew build
+
 docker build -t adron/hasura-spring-boot-graphql .
 docker run -p 8081:8080 adron/hasura-spring-boot-graphql
+
+
+
+# docker container rm adron/hasura-spring-boot-graphql
+# docker image rm adron/hasura-spring-boot-graphql -f
+
